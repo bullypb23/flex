c4822b967ff6e3450ce1ec5c94458e59fed9f94c
Vladimir Bulatovic <vladimir.bulatovic@thingsolver.com>
2021-04-03 15:04:54 +0200

    modified header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,51 @@
-import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import { Row, Container, Navbar, Nav } from "react-bootstrap";
+import PhoneIcon from '@material-ui/icons/Phone';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = () => (
+  <Navbar variant="light" expand="lg" id="navbar">
+    <Container className="container">
+      <Link to="/">
+        <Navbar.Brand>
+          <StaticImage src="../images/logo.jpg" alt="Flex Moving Logo" width={90} />
+        </Navbar.Brand>
+      </Link>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse>
+        <Container className="navbar-container">
+          <Row>
+            <PhoneIcon />Phone number
+          </Row>
+          <Row>
+            <Nav>
+              <Link to="/" className="link-items">
+                <Nav.Item>
+                  Home
+                </Nav.Item>
+              </Link>
+              <Link to="/our-story" className="link-items">
+                <Nav.Item>
+                  Our story
+                </Nav.Item>
+              </Link>
+              <Link to="/moving-services" className="link-items">
+                <Nav.Item>
+                  Moving services
+                </Nav.Item>
+              </Link>
+              <Link to="/contact" className="link-items">
+                <Nav.Item>
+                  Contact
+                </Nav.Item>
+              </Link>
+            </Nav>
+          </Row>
+        </Container>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+);
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+export default Header;
