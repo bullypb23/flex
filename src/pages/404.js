5dbffb210a9dcb5989b0a385bfbcf64f404d32cf
Vladimir Bulatovic <vladimir.bulatovic@thingsolver.com>
2021-05-13 22:31:58 +0200

    changed static image to img element

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,15 @@
-import * as React from "react"
+import * as React from "react";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import { Link } from "gatsby";
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>Go back to <Link to="/">Home page.</Link></p>
   </Layout>
 )
 
