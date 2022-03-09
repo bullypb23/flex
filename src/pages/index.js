c52834afcfdd67d8f41167d534cb4fb598bd4cff
Vladimir Bulatovic <vladimir.bulatovic@thingsolver.com>
2022-03-09 17:23:19 +0100

    commented links

--- src/pages/index.js
+++ src/pages/index.js
@@ -92,7 +92,7 @@ const IndexPage = () => {
           <Typography className="service-paragraph">As the country responds to the pandemic of COVID-19, our thoughts are with the people affected and the medical professionals working around the clock to help those most in need. At Flex Moving Company, we are working to do our part as an ESSENTIAL BUSINESS by ensuring the safety of our employees, striving to protect the health and well-being of the communities in which we operate and providing relocation services and resources to our customers to make their move as safe as possible. We have taken great care in following the guidelines provided by the CDC to ensure our facilities, staff and equipment are safe and sanitary.</Typography>
         </Box>
       </Box>
-      <Box className="info-container">
+      {/* <Box className="info-container">
         <Box style={{ width: '80%' }}>
           <h1 className="data-heading">Follow us on:</h1>
           <Box className="logo-wrapper">
@@ -118,7 +118,7 @@ const IndexPage = () => {
             </Box>
           </Box>
         </Box>
-      </Box>
+      </Box> */}
     </Layout>
   )
 }
